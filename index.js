c4ac3dc4542471a5948ff2ec5746a8980a9449cf
Sushil Magar <sushilmagar3@gmail.com>
2018-11-28 22:56:35 -0800

    Some progress

--- index.js
+++ index.js
@@ -1,3 +1,79 @@
 #!/usr/bin/env node
 
-console.log('Got here');
+const program = require('commander');
+const fs = require('fs');
+
+program
+    .arguments('<firstFileName>') 
+    .arguments('<secondFileName>')
+    .option('-1, --firstField <n>', 'An integer argument', parseInt)
+    .option('-2, --secondField <n>', 'An integer argument', parseInt)
+    .action(function(firstFileName, secondFileName, fields, field) {
+        try {
+            const readFile = (filename) => fs.readFileSync(filename, {encoding: 'utf8'}).split('\n');
+            
+            // Read file content from both files
+            const firstFileContent = readFile(firstFileName);
+            const secondFileContent = readFile(secondFileName);
+
+            console.log(firstFileContent)
+            console.log('=======')
+            console.log(secondFileContent)
+            console.log('=======')
+            console.log(program.firstField)
+            console.log(program.secondField)
+
+            // If common elements exists
+            if (firstFileContent && secondFileContent) {
+                let row = 0;
+                let isFileEnd = false;
+                
+                while (!isFileEnd) {
+                    let fileOneContent;
+                    let fileTwoContent;
+                    
+                    if(firstFileContent[row] && secondFileContent[row]) {
+                        fileOneContent = firstFileContent[row].split(' ');
+                        fileTwoContent = secondFileContent[row].split(' ');
+
+                        let column = 0;
+
+                        if (fileOneContent[column] === fileTwoContent[column]) {
+                            let result = [];
+                            let isFileRowEnd = false;
+        
+                            result.push(fileOneContent[column]);
+                            column++;
+        
+                            while (!isFileRowEnd) {
+                                const fileOne = fileOneContent[column];
+                                const fileTwo = fileTwoContent[column];
+        
+                                if (fileOne && fileTwo) {
+                                    result.push(fileOne, fileTwo);
+                                    column++;
+                                } else if (fileOne && !fileTwo) {
+                                    result.push(fileOne);
+                                    column++;
+                                } else if (!fileOne && fileTwo) {
+                                    result.push(fileTwo);
+                                    column++;
+                                } else {
+                                    isFileRowEnd = true;
+                                    row++;
+                                }
+                            }
+        
+                            console.log(result);
+                        } else {
+                            isFileEnd = true;
+                        }
+                    }
+                }
+            }
+        } catch (err) {
+            console.log(err)
+        }        
+    })
+    .parse(process.argv);
+
