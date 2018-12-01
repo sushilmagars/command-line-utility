97c054aa2506d922f091d5d6a819a74b8bf53d85
Sushil Magar <sushilmagar3@gmail.com>
2018-11-30 19:39:52 -0800

    Changed text files

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const fs = require('fs');
 program
     .arguments('<firstFileName>') 
     .arguments('<secondFileName>')
-    .option('-1, --firstFlag <n>', 'An integer argument', Number.parseInt)
-    .option('-2, --secondFlag <n>', 'An integer argument', Number.parseInt)
+    .option('-1, --firstfilestartposition <n>', 'An integer argument', Number.parseInt)
+    .option('-2, --secondfilestartposition <n>', 'An integer argument', Number.parseInt)
     .action(function(firstFileName, secondFileName) {
         try {
             const readFile = (filename) => fs.readFileSync(filename, {encoding: 'utf8'}).split('\n');
@@ -30,12 +30,12 @@ program
                         let fileOneContent = splitRow(firstFileContent[row]);
                         let fileTwoContent = splitRow(secondFileContent[row]);
 
-                        if (program.firstFlag > 1) {
-                            fileOneContent = sliceRow(fileOneContent, program.firstFlag - 1);
+                        if (program.firstfilestartposition > 1) {
+                            fileOneContent = sliceRow(fileOneContent, program.firstfilestartposition - 1);
                         }
 
-                        if (program.secondFlag > 1) {
-                            fileTwoContent = sliceRow(fileTwoContent, program.secondFlag - 1);
+                        if (program.secondfilestartposition > 1) {
+                            fileTwoContent = sliceRow(fileTwoContent, program.secondfilestartposition - 1);
                         }
 
                         let column = 0;
